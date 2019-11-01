21925840c625390727e374eae9180f9853ca8d42
Rosa Román <rosaromanalvarez@gmail.com>
2019-11-01 16:06:09 +0100

    Add files via upload

---
+++ WeeklyTests/take_screenshot_on_failure.py
@@ -0,0 +1,22 @@
+import sys
+import unittest
+from selenium import webdriver
+
+
+class ScreenShotOnFailure(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def tearDown(self):
+        if sys.exc_info()[0]:
+            self.driver.save_screenshot("failshot_%s.png" % self._testMethodName)
+        self.driver.quit()
+
+    def test_example_1(self):
+        driver = self.driver
+        driver.get('http://the-internet.herokuapp.com')
+        assert driver.title == 'blah blah blah'
+
+if __name__ == "__main__":
+    unittest.main()
